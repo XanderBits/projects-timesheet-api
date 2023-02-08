ed74073920abc228b64e04850ff4accc788bdafb
XanderBits <amarettosyntax@protonmail.com>
2023-02-08 19:26:07 -0400

    [ADD] Role Entiy

---
+++ src/role/entities/role.entity.ts
@@ -0,0 +1,14 @@
+import { User } from 'src/auth/entities/user.entity';
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Role {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+ 
+  @Column('text', {unique: true})
+  name: string;
+ 
+  @ManyToMany (() => User, user => user.role_ids)
+  user_ids: User[]
+}
