33cedba4b423271191d18440aed75aaba06c7b8e
XanderBits <amarettosyntax@protonmail.com>
2023-02-21 23:25:03 -0400

    [ADD] PASSPORT - JWT Config to AUTH

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,11 +5,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Project } from 'src/project/entities/project.entity';
 import { Role } from 'src/role/entities/role.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategies';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([ User, Project, Role])],
-  exports: [TypeOrmModule]
+  providers: [AuthService, JwtStrategy],
+  imports: [
+            TypeOrmModule.forFeature([ User, Project, Role ]),
+            PassportModule.register( { defaultStrategy: 'jwt' } ),
+            JwtModule.registerAsync({
+              useFactory: () => { 
+                return {
+                  secret: process.env.JWT_SECRET,
+                  signOptions: {
+                    expiresIn: '3h'
+                  }
+                }
+              }
+            })
+  ],
+  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule]
 })
 export class AuthModule {}
