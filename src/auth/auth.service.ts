1214e9195dea5a1f7e58d1fb10f268911adc2531
XanderBits <amarettosyntax@protonmail.com>
2023-02-22 22:54:26 -0400

    [ADD] JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,8 @@ import { Repository } from 'typeorm';
 import { CreateUserDto , LoginUserDto } from './dto/index';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt'; 
+import { JwtPayload } from './interfaces/jwt-payload.interfaces';
+import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
@@ -13,28 +15,49 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     @InjectRepository(Role)
-    private readonly roleRepository: Repository<Role>
-  ){}
-
-  async create(createUserDto: CreateUserDto): Promise<User>{
-    try{
-      const {role_ids, password, ...userData} = createUserDto
-      const emailExists = await this.userRepository.findOneBy({email: userData.email});
-      if(emailExists !== null) throw new HttpException(`Email address ${userData.email} already exists`, HttpStatus.BAD_REQUEST);
-
-      const role = await this.roleRepository.findBy({id: role_ids})
-      if(role.length == 0) throw new NotFoundException("The provided Role doesnt exists")
-      const user = this.userRepository.create({...userData, 
-                                              password: bcrypt.hashSync(password, 10), 
-                                              role_ids: role});
-      await this.userRepository.save(user)
-      delete user.password;
-      return user
-
-    }catch(error){
-      return error
-    }
+    private readonly roleRepository: Repository<Role>,
+    
+    private readonly jwtService: JwtService,
+    ){}
+    
+    
+  private async getJwtToken(payload: JwtPayload){
+    const token = this.jwtService.sign( payload );
+    console.log(token)
+    return token
   }
+  
+  async create(createUserDto: CreateUserDto) {
+  try{
+    const {role_ids, password, ...userData} = createUserDto
+    userData.email = userData.email.toLowerCase();
+    const emailExists = await this.userRepository.findOneBy({email: userData.email});
+    if(emailExists !== null) throw new HttpException(`Email address ${userData.email} already exists`, HttpStatus.BAD_REQUEST);
+
+    const role = await this.roleRepository.findBy({id: role_ids})
+    if(role.length == 0) throw new NotFoundException("The provided Role doesnt exists")
+    const user = this.userRepository.create({...userData, 
+                                            password: bcrypt.hashSync(password, 10), 
+                                            role_ids: role});
+  
+    await this.userRepository.save(user)
+    delete user.password;
+
+    const userWithToken = {
+      ...user, 
+      token: await this.getJwtToken({
+        id: user.id,
+        name: user.name,
+        lastname: user.lastname,
+        user_type: user.user_type,
+      })
+    };
+    return userWithToken
+
+  }catch(error){
+    return error
+  }
+}
 
   async login(loginUserDto: LoginUserDto){
     try{
@@ -48,7 +71,16 @@ export class AuthService {
 
       if( !bcrypt.compareSync(password, user.password) ) throw new UnauthorizedException("Incorrect  email or password")
       
-      return user;
+      const userWithToken = {
+        ...user, 
+        token: await this.getJwtToken({
+          id: user.id,
+          name: user.name,
+          lastname: user.lastname ,
+          user_type: user.user_type
+        })
+      };
+      return userWithToken;  
     }catch(error){
       return error
     }
