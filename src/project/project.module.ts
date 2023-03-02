a9ac60422518743ba463a483d969ef4124986c92
XanderBits <amarettosyntax@protonmail.com>
2023-03-01 20:22:17 -0400

    [ADD] AuthModule Import

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -4,10 +4,11 @@ import { ProjectController } from './project.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/auth/entities/user.entity';
 import { Project } from './entities/project.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ProjectController],
   providers: [ProjectService],
-  imports: [TypeOrmModule.forFeature([ User, Project ])]
+  imports: [TypeOrmModule.forFeature([ User, Project ]), AuthModule]
 })
 export class ProjectModule {}
--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -4,10 +4,11 @@ import { RoleController } from './role.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/auth/entities/user.entity';
 import { Role } from './entities/role.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [RoleController],
   providers: [RoleService],
-  imports: [TypeOrmModule.forFeature([ User, Role])]
+  imports: [TypeOrmModule.forFeature([ User, Role]), AuthModule]
 })
 export class RoleModule {}
