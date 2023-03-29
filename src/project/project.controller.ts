59aec6860f06149896e37f3c58813d94569f99e2
XanderBits <amarettosyntax@protonmail.com>
2023-03-29 18:39:42 -0400

    [ADD] User-role guard to controllers

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -3,6 +3,9 @@ import { ProjectService } from './project.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { RoleProtected } from 'src/auth/decorators/role-protected/role-protected.decorator';
+import { UserRoleGuard } from 'src/auth/guards/user-role/user-role.guard';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @Controller('project')
 @UseGuards( AuthGuard( ) )
@@ -10,6 +13,8 @@ export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
 
   @Post()
+  @RoleProtected(ValidRoles.admin)
+  @UseGuards(UserRoleGuard)
   create(@Body() createProjectDto: CreateProjectDto) {
     return this.projectService.create(createProjectDto);
   }
@@ -25,11 +30,15 @@ export class ProjectController {
   }
 
   @Patch(':id')
+  @RoleProtected(ValidRoles.admin)
+  @UseGuards(UserRoleGuard)
   update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDto) {
     return this.projectService.update(id, updateProjectDto);
   }
 
   @Delete(':id')
+  @RoleProtected(ValidRoles.admin)
+  @UseGuards(UserRoleGuard)
   remove(@Param('id') id: string) {
     return this.projectService.remove(id);
   }
--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -3,6 +3,9 @@ import { RoleService } from './role.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { UserRoleGuard } from 'src/auth/guards/user-role/user-role.guard';
+import { ValidRoles } from 'src/auth/interfaces';
+import { RoleProtected } from 'src/auth/decorators/role-protected/role-protected.decorator';
 
 @Controller('role')
 @UseGuards( AuthGuard( ) )
@@ -10,6 +13,8 @@ export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
   @Post()
+  @RoleProtected(ValidRoles.admin)
+  @UseGuards(UserRoleGuard)
   create(@Body() createRoleDto: CreateRoleDto) {
     return this.roleService.create(createRoleDto);
   }
@@ -25,11 +30,15 @@ export class RoleController {
   }
 
   @Patch(':id')
+  @RoleProtected(ValidRoles.admin)
+  @UseGuards(UserRoleGuard)
   update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
     return this.roleService.update(id, updateRoleDto);
   }
 
   @Delete(':id')
+  @RoleProtected(ValidRoles.admin)
+  @UseGuards(UserRoleGuard)
   remove(@Param('id') id: string) {
     return this.roleService.remove(id);
   }
